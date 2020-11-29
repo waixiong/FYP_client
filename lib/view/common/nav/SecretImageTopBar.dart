60574e99658473087884cae2d1ab0cbdc27e68b2
CarlosSia <siasimcheong@gmail.com>
2020-11-30 00:41:05 +0800

    Secret Image

---
+++ lib/view/common/nav/SecretImageTopBar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../pages/DecodeImagePage.dart';
+import '../../pages/GenerateImagePage.dart';
+
+class SecretImageTopBar extends StatefulWidget {
+  @override
+  _SecretImageTopBarState createState() => new _SecretImageTopBarState();
+}
+
+class _SecretImageTopBarState extends State<SecretImageTopBar>{
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Secret Image", textAlign: TextAlign.center, style: TextStyle(fontSize: 30),),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Column(
+          children: <Widget>[
+            // the tab bar with two items
+            SizedBox(
+              height: 50,
+              child: AppBar(
+                bottom: TabBar(
+                  unselectedLabelColor: Colors.white,
+                  labelColor: Colors.black,
+                  indicatorColor: Colors.black,
+                  tabs: [
+                    Tab(
+                      icon: Text("Generate Image", style: TextStyle(fontSize: 24),),
+                    ),
+                    Tab(
+                      icon: Text("Decode Image", style: TextStyle(fontSize: 24),),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            // create widgets for each tab bar here
+            Expanded(
+              child: TabBarView(
+                children: [
+                  // first tab bar view widget
+                  GenerateImagePage(),
+
+                  // second tab bar view widget
+                  DecodeImagePage(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/pages/GenerateImagePage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class DecodeImagePage extends StatefulWidget {
+  @override
+  DecodeImagePageState createState() => new DecodeImagePageState();
+}
+
+class DecodeImagePageState extends State<DecodeImagePage>{
+
+  String decodedOutput = "";
+
+  void onChange(String value){
+    setState(() => decodedOutput = value);
+  }
+
+  void dummyOnChange(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: [
+                    Expanded(
+                      child: new Text(
+                        decodedOutput, maxLines: 5,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 16,),
+                new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 180,
+                      height: 180,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff7c94b6),
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 8,
+                        ),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 16,),
+                new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    new RaisedButton(
+                      onPressed: dummyOnChange,
+                      color: Colors.white,
+                      disabledColor: Colors.white,
+                      child: new Text("Decode From Gallery",
+                        style: TextStyle(
+                          color: Colors.lightGreen,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
